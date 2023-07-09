434eb40ec8e47f2b18a03135e52908215dc64d30
Sagar Nivargi <66205757+sagarnivargi@users.noreply.github.com>
2023-07-09 22:11:31 +0530

    Add files via upload

---
+++ EmployeePage/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    EmployeeDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
